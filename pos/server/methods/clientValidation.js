84f2f93aa515669d532e465718b397b1945dd512
phirum <phirumsim@gmail.com>
2017-06-16 16:04:22 +0700

    update findItem function

--- pos/server/methods/clientValidation.js
+++ pos/server/methods/clientValidation.js
@@ -2,8 +2,60 @@ import {Item} from '../../imports/api/collections/item.js'
 import {RingPullInventories} from '../../imports/api/collections/ringPullInventory.js'
 
 Meteor.methods({
-    findItem(itemId){
-        return Item.findOne(itemId);
+    findItems(ids){
+        return Item.aggregate([
+            {$match:{_id:{$in:ids}}},
+            {
+                $lookup: {
+                    from: "units",
+                    localField: "unitId",
+                    foreignField: "_id",
+                    as: "unitDoc"
+                },
+
+            }, {
+                $unwind: {path: "$unitDoc", preserveNullAndEmptyArrays: true}
+            }
+            , {
+                $lookup: {
+                    from: "pos_categories",
+                    localField: "categoryId",
+                    foreignField: "_id",
+                    as: "categoryDoc"
+                },
+
+            }, {
+                $unwind: {path: "$categoryDoc", preserveNullAndEmptyArrays: true}
+            }
+        ]);
+    },
+    findItem(code,type){
+        let selector = type=="id" ? {_id: code} : {barcode: code};
+        return Item.aggregate([
+            {$match:selector},
+            {
+                $lookup: {
+                    from: "units",
+                    localField: "unitId",
+                    foreignField: "_id",
+                    as: "unitDoc"
+                },
+
+            }, {
+                $unwind: {path: "$unitDoc", preserveNullAndEmptyArrays: true}
+            }
+            , {
+                $lookup: {
+                    from: "pos_categories",
+                    localField: "categoryId",
+                    foreignField: "_id",
+                    as: "categoryDoc"
+                },
+
+            }, {
+                $unwind: {path: "$categoryDoc", preserveNullAndEmptyArrays: true}
+            }
+        ])[0];
     },
     checkStockByLocation(stockLocationId, items){
         let result = {isEnoughStock: true, message: ''};
